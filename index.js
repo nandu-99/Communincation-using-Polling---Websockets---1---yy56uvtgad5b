132ef8d6c465b6a5fdce797b0c3d0a42b26cf0d7
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2024-11-28 19:46:05 +0530

    added files

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ app.get("/", (_, res) => {
 
 const messages = [];
 
+app.post("/message", async(req, res)=>{
+  const {text, user} = req.body 
+  if(!text || !user){
+    return res.status(400).json({ "error": "Please provide a valid input" })
+  } 
+  const timestamp = new Date().toISOString(); 
+  const mes =  { "user": user, "text": text, "timestamp": timestamp }
+  messages.push(mes)
+  return res.status(400).json({ "message": mes})
+})
+
+app.get("/findMessages", (req, res)=>{
+  return res.status(200).json({messages: messages})
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
